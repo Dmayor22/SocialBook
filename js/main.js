52f59ce1682fd266a18bd22a79d1adc70b7f440f
Dmayor22 <oluwamuyiwasogbetun@gmail.com>
2026-08-06 13:46:01 +0100

    app

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 // variables
 const menuBtn = document.querySelector(".profile_click");
 const menuDropdown = document.querySelector(".dropdown_menu");
+const body = document.querySelector("body");
+const darkModeBtn = document.querySelector("#dark_mode_toogle");
 
+// handles menu items when the user profile is clicked
 menuBtn.addEventListener("click", () => {
   menuDropdown.classList.toggle("click");
+});
+
+// handles the darkmode and light mode
+darkModeBtn.addEventListener("click", () => {
+  darkModeBtn.classList.toggle("dark_btn_on");
+  body.classList.toggle("dark");
 });
