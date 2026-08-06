08d2ad842e1a3a79b31a4b34aa900d979ceaa6af
Dmayor22 <oluwamuyiwasogbetun@gmail.com>
2026-08-06 12:56:33 +0100

    app

--- js/main.js
+++ js/main.js
@@ -3,6 +3,5 @@ const menuBtn = document.querySelector(".profile_click");
 const menuDropdown = document.querySelector(".dropdown_menu");
 
 menuBtn.addEventListener("click", () => {
-  //   menuDropdown.style.display = "block";
   menuDropdown.classList.toggle("click");
 });
