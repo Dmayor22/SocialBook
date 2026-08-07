f72da621f5c91bb03ec10c4be74ad17b731b15e6
Dmayor22 <oluwamuyiwasogbetun@gmail.com>
2026-08-07 04:29:02 +0100

    app

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const newsFeed = document.querySelectorAll(".news_feed");
 const userPost = document.querySelector(".user_post");
 const rightSide = document.querySelector(".right-sidebar");
 const darkModeBtn = document.querySelector("#dark_mode_toogle");
+const dropdownUserProfile = document.querySelectorAll(".dropdown_user_profile");
 
 // handles menu items when the user profile is clicked
 menuBtn.addEventListener("click", () => {
@@ -18,10 +19,62 @@ darkModeBtn.addEventListener("click", () => {
   darkModeBtn.classList.toggle("dark_btn_on");
   body.classList.toggle("dark");
   header.classList.toggle("dark");
+
+  // array
   newsFeed.forEach((newsfeed) => {
     newsfeed.classList.toggle("dark");
   });
   userPost.classList.toggle("dark");
   rightSide.classList.toggle("dark");
+  menuDropdown.classList.toggle("dark");
+
+  // array
+  dropdownUserProfile.forEach((userProfile) => {
+    userProfile.classList.toggle("dark");
+  });
+
+  // update localstorage
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 });
 
+// save dark mode preference to local storage
+if (localStorage.getItem("theme") == "light") {
+  darkModeBtn.classList.remove("dark_btn_on");
+  body.classList.remove("dark");
+  header.classList.remove("dark");
+
+  // array
+  newsFeed.forEach((newsfeed) => {
+    newsfeed.classList.remove("dark");
+  });
+  userPost.classList.remove("dark");
+  rightSide.classList.remove("dark");
+  menuDropdown.classList.remove("dark");
+
+  // array
+  dropdownUserProfile.forEach((userProfile) => {
+    userProfile.classList.remove("dark");
+  });
+} else if (localStorage.getItem("theme") == "dark") {
+  darkModeBtn.classList.add("dark_btn_on");
+  body.classList.add("dark");
+  header.classList.add("dark");
+  // array
+  newsFeed.forEach((newsfeed) => {
+    newsfeed.classList.add("dark");
+  });
+  userPost.classList.add("dark");
+  rightSide.classList.add("dark");
+  menuDropdown.classList.add("dark");
+
+  // array
+  dropdownUserProfile.forEach((userProfile) => {
+    userProfile.classList.add("dark");
+  });
+} else {
+  localStorage.setItem("theme", "light");
+}
